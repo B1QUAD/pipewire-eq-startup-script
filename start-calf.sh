aadc88072fa1452f3ccce9b7b6feeb84a6f628d7
David <54114451+B1QUAD@users.noreply.github.com>
2022-02-13 06:17:17 +0000

    Update start-calf.sh

--- start-calf.sh
+++ start-calf.sh
@@ -2,14 +2,17 @@
 CALFFIRSTIN1="Calf Studio Gear:Equalizer 12 Band In #1"		#Edit as needed.
 CALFFIRSTIN2="Calf Studio Gear:Equalizer 12 Band In #2"		#Edit as needed.
 
+CALFANALYZER1="Calf Studio Gear:Analyzer In #1"
+CALFANALYZER2="Calf Studio Gear:Analyzer In #2"
+
 CALFLASTOUT1="Calf Studio Gear:Equalizer 12 Band Out #1"	#Edit as needed.
 CALFLASTOUT2="Calf Studio Gear:Equalizer 12 Band Out #2"	#Edit as needed.
 
-ACTUALOUTPUTHARDWARE1="K3:playback_FL"				#Edit as needed.
-ACTUALOUTPUTHARDWARE2="K3:playback_FR"				#Edit as needed.
+ACTUALOUTPUTHARDWARE1="USB-C to 3.5mm Headphone Jack Adapter Analog Stereo:playback_FL"				#Edit as needed.
+ACTUALOUTPUTHARDWARE2="USB-C to 3.5mm Headphone Jack Adapter Analog Stereo:playback_FR"				#Edit as needed.
 
 #These probably need not be changed, though feel free to anyway.
-NODENAME=EQ #Name of virtual device
+NODENAME="sink Monitor" #Name of virtual device
 PWLINKORJACKCONNECT="pw-jack jack_connect" #Replacing "pw-jack jack_connect" with "pw-link" may be possible for Pipewire 0.3.26 and above
 CHECKEDPORT=$CALFFIRSTIN2
 CHECKEDPORT2=$ACTUALOUTPUTHARDWARE2
@@ -18,10 +21,10 @@ VIRTUALMONITOR1="$NODENAME:monitor_FL"
 VIRTUALMONITOR2="$NODENAME:monitor_FR"
 
 #1 Create virtual device unless it"s already there.
-if (pw-jack jack_lsp | grep -q "$NODENAME"); then
+if (pw-jack jack_lsp | grep -q $NODENAME); then
     echo "nothing to be done."
 else
-	pw-cli create-node adapter { factory.name=support.null-audio-sink node.name="$NODENAME" media.class=Audio/Sink object.linger=1 audio.position=[ FL FR ] }	#COMMENT OUT if Pipewire is older than 0.3.25, and uncomment the line below
+	pw-cli create-node adapter { factory.name=support.null-audio-sink node.name=$NODENAME media.class=Audio/Sink object.linger=1 audio.position=[ FL FR ] }	#COMMENT OUT if Pipewire is older than 0.3.25, and uncomment the line below
 #	pw-cli create-node adapter { factory.name=support.null-audio-sink node.name="$NODENAME" media.class=Audio/Sink object.linger=1 audio.position=FL,FR }		#UNCOMMENT IF you comment out the above. Only for Pipewire UP TO 0.3.25
 fi
 
@@ -29,7 +32,7 @@ fi
 if (pw-jack jack_lsp | grep -q "$CALFLASTOUT2"); then
 	echo "nothing to be done."
 else
-	calfjackhost eq12:preset-1 &
+	calfjackhost eq12:dt770 --load ~/audio/eq-plugins &
 #	lsp-plugins-para-equalizer-x16-stereo -c /tmp/preset.cfg &
 fi
 
@@ -48,6 +51,8 @@ done
 #4 Connect Jack ports.
 ($PWLINKORJACKCONNECT "$ACTUALOUTPUTHARDWARE1" "$CALFLASTOUT1" ;
 $PWLINKORJACKCONNECT "$ACTUALOUTPUTHARDWARE2" "$CALFLASTOUT2" ;
+$PWLINKORJACKCONNECT "$VIRTUALMONITOR1" "$CALFANALYZER1" ;
+$PWLINKORJACKCONNECT "$VIRTUALMONITOR2" "$CALFANALYZER2" ;
 $PWLINKORJACKCONNECT "$VIRTUALMONITOR1" "$CALFFIRSTIN1" ;
 $PWLINKORJACKCONNECT "$VIRTUALMONITOR2" "$CALFFIRSTIN2" )&
 
